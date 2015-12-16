7ab3e65617adf9f203ab51cd4b42b1dea6a5e87c
Matthew Lake <mgtlake@outlook.com>
2015-12-16 13:24:42 +1000

    Done part 1

--- day_2.hs
+++ day_2.hs
@@ -1,8 +1,18 @@
 module Main where
+import Data.List.Split
 
 main :: IO ()
 main = do
 	input <- readFile("day_2.in")
-	putStrLn $ "Part 1: " ++ input
-	where
-		part_1 = "test"
+	let lines = [line | line <- (splitOn "\n" input), length line /= 0]
+	let total = sum [paper line | line <- lines]
+	putStrLn $ "Part 1: " ++ show total
+
+paper :: String -> Int
+paper line = sum [2*side | side <- sides] + minimum sides
+	where 
+		ints = [read c :: Int | c <- (splitOn "x" line)]
+		l = ints !! 0
+		w = ints !! 1
+		h = ints !! 2
+		sides = [l*w, w*h, h*l]
