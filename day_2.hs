ebe6e6a031c79366040a6d1394cc05080791a410
Matthew Lake <mgtlake@outlook.com>
2015-12-16 17:15:27 +1000

    Day 2 part 2

--- day_2.hs
+++ day_2.hs
@@ -1,12 +1,15 @@
 module Main where
 import Data.List.Split
+import Data.List
 
 main :: IO ()
 main = do
 	input <- readFile("day_2.in")
 	let lines = [line | line <- (splitOn "\n" input), length line /= 0]
 	let total = sum [paper line | line <- lines]
+	let length = sum [ribbon line | line <- lines]
 	putStrLn $ "Part 1: " ++ show total
+	putStrLn $ "Part 2: " ++ show length
 
 paper :: String -> Int
 paper line = sum [2*side | side <- sides] + minimum sides
@@ -16,3 +19,12 @@ paper line = sum [2*side | side <- sides] + minimum sides
 		w = ints !! 1
 		h = ints !! 2
 		sides = [l*w, w*h, h*l]
+
+ribbon :: String -> Int
+ribbon line = sum [2*side | side <- (drop 1 (reverse (sort ints)))] + bow
+	where
+		ints = [read c :: Int | c <- (splitOn "x" line)]
+		l = ints !! 0
+		w = ints !! 1
+		h = ints !! 2
+		bow = l*w*h
