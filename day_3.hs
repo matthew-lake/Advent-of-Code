27a3b8f7ef97083249f99c21fea42eb2f7898331
Matthew Lake <mgtlake@outlook.com>
2015-12-16 18:55:35 +1000

    Day 3 part 2

--- day_3.hs
+++ day_3.hs
@@ -6,9 +6,17 @@ main :: IO ()
 main = do
 	input <- readFile("day_3.in")
 	let cleanInput = [c | c <- input, c == '<' || c == '>' || c == 'v' || c == '^']
+
 	let sublists = [take x cleanInput | x <- [1..(length cleanInput)]]
 	let visited = nub [position s (0,0) | s <- sublists]
-	putStrLn $ "Part 1: " ++ show (length visited)
+	putStrLn $ "Part 1: " ++ show (length visited)	
+
+	let santa = [cleanInput !! x | x <- [0..((length cleanInput) - 1)], odd x]
+	let robot = [cleanInput !! x | x <- [0..((length cleanInput) - 1)], even x]
+	let visitedSanta = nub [position s (0,0) | s <- [take x santa | x <- [1..(length santa)]]]
+	let visitedRobot = nub [position s (0,0) | s <- [take x robot | x <- [1..(length robot)]]]
+	let both = nub (visitedSanta ++ visitedRobot)
+	putStrLn $ "Part 2: " ++ show (length both)
 
 position :: [Char] -> (Int, Int) -> (Int, Int)
 position [c] (a,b)
